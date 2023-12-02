89e500cd6870fb478dad66b5dd622a671a76aa9b
Matt Kaye <mrkaye97@gmail.com>
2023-12-02 14:05:16 -0500

    python to confirm

---
+++ python/day1.py
@@ -0,0 +1,18 @@
+with open("data/day1.txt", "r") as f:
+    data = f.read().split("\n")
+
+def find_digit(s: str) -> int:
+    for char in s:
+        if char in map(lambda x: str(x), range(10)):
+            return char
+
+    return ""
+
+result = sum(
+    map(
+        lambda line: int(find_digit(line) + find_digit(line[::-1])),
+        data
+    )
+)
+
+print(result)
