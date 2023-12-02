04c12c2bcf55a65fb039ed70b2a4536b4f1d9c1a
Matt Kaye <mrkaye97@gmail.com>
2023-12-02 13:51:11 -0500

    more progress

--- advent-of-code/src/Day1.hs
+++ advent-of-code/src/Day1.hs
@@ -7,28 +7,34 @@ import Data.List (find)
 
 main :: IO ()
 main = do
-    -- s <- readFile "src/data/day1.txt"
-    -- doSomethingWith s
-    print $ findFirstLastPair "xy24"
+    s <- readFile "src/data/day1.txt"
+
+    print $ x
+    where
+        x = map findFirstLastPair s
 
 -- doSomethingWith :: String -> IO ()
 -- doSomethingWith str = print $ splitInputByLine str
 
 -- splitInputByLine x = splitOn "\n" x
 
-findFirstLastPair str = x ++ y
-    where
-        x = case findFirstDigit str of
-            Just z -> [z]
-            _ -> ""
-        y = case findLastDigit str of
-            Just z -> [z]
-            _ -> ""
+findFirstLastPair :: String -> Maybe Int
+findFirstLastPair str = readMaybe (x ++ y)
+  where
+    x = case findFirstDigit str of
+        Just z -> [z]
+        _ -> ""
+    y = case findLastDigit str of
+        Just z -> [z]
+        _ -> ""
 
+findFirstDigit :: String -> Maybe Char
 findFirstDigit str = find isDigit str
+
+findLastDigit :: String -> Maybe Char
 findLastDigit str = find isDigit $ reverse str
 
 isDigit :: Char -> Bool
 isDigit x = case readMaybe [x] :: Maybe Int of
-    Just y | y `elem` [0 .. 9]  -> True
-    _                           -> False
+    Just y | y `elem` [0 .. 9] -> True
+    _ -> False
