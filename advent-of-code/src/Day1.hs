627839fd5af310d58e4146fd700d738ca1c4e42a
Matt Kaye <mrkaye97@gmail.com>
2023-12-02 13:49:02 -0500

    progress

--- advent-of-code/src/Day1.hs
+++ advent-of-code/src/Day1.hs
@@ -2,23 +2,33 @@ module Day1 where
 
 import Data.List.Split (splitOn)
 import Data.Maybe
-import Data.Text (find)
 import Text.Read (readMaybe)
+import Data.List (find)
 
 main :: IO ()
 main = do
     -- s <- readFile "src/data/day1.txt"
     -- doSomethingWith s
-    print $ isDigit "9"
+    print $ findFirstLastPair "xy24"
 
-doSomethingWith :: String -> IO ()
-doSomethingWith str = print $ splitInputByLine str
+-- doSomethingWith :: String -> IO ()
+-- doSomethingWith str = print $ splitInputByLine str
 
-splitInputByLine x = splitOn "\n" x
+-- splitInputByLine x = splitOn "\n" x
 
-findFirstDigit :: [Char] -> Int
+findFirstLastPair str = x ++ y
+    where
+        x = case findFirstDigit str of
+            Just z -> [z]
+            _ -> ""
+        y = case findLastDigit str of
+            Just z -> [z]
+            _ -> ""
+
 findFirstDigit str = find isDigit str
+findLastDigit str = find isDigit $ reverse str
 
-isDigit x = case readMaybe x :: Maybe Int of
+isDigit :: Char -> Bool
+isDigit x = case readMaybe [x] :: Maybe Int of
     Just y | y `elem` [0 .. 9]  -> True
     _                           -> False
