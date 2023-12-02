62406746ee03457d715ffecb257186108f416e36
Matt Kaye <mrkaye97@gmail.com>
2023-12-02 13:29:59 -0500

    more mucking around

--- advent-of-code/src/Day1.hs
+++ advent-of-code/src/Day1.hs
@@ -1,6 +1,24 @@
+module Day1 where
+
+import Data.List.Split (splitOn)
+import Data.Maybe
+import Data.Text (find)
+import Text.Read (readMaybe)
+
+main :: IO ()
 main = do
-    s <- readFile "src/data/day1.txt"
-    doSomethingWith s
+    -- s <- readFile "src/data/day1.txt"
+    -- doSomethingWith s
+    print $ isDigit "9"
 
 doSomethingWith :: String -> IO ()
-doSomethingWith str = putStrLn str
+doSomethingWith str = print $ splitInputByLine str
+
+splitInputByLine x = splitOn "\n" x
+
+findFirstDigit :: [Char] -> Int
+findFirstDigit str = find isDigit str
+
+isDigit x = case readMaybe x :: Maybe Int of
+    Just y | y `elem` [0 .. 9]  -> True
+    _                           -> False
