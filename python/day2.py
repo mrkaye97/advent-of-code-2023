7dd7f35e6ad1af74502498456970f28a2d9a1008
Matt Kaye <mrkaye97@gmail.com>
2023-12-03 10:41:08 -0500

    functional day 2 solution

--- python/day2.py
+++ python/day2.py
@@ -11,11 +11,13 @@ class Color(Enum):
     GREEN = "green"
     RED = "red"
 
+
 color_limits = {
     Color.BLUE: 14,
     Color.RED: 12,
     Color.GREEN: 13,
 }
+
 
 @dataclass
 class Draw:
@@ -23,15 +25,18 @@ class Draw:
     green: int
     red: int
 
+
 @dataclass
 class Game:
     id: int
     draws: List[Draw]
 
+
 def get_color_count(draw: str, color: Color) -> int:
     for color_count in draw.split(","):
         if color.value in color_count.strip():
             return int(color_count.strip().split(" ")[0].strip())
+
 
 def parse_draw(draw: str) -> Draw:
     return Draw(
@@ -40,24 +45,29 @@ def parse_draw(draw: str) -> Draw:
         red=get_color_count(draw, Color.RED),
     )
 
+
 def parse_game(record: str) -> Game:
     name_id, records = record.split(":")
     id = name_id.split(" ")[1]
     draws = [d.strip() for d in records.split(";")]
 
-    return Game(
-        id=int(id),
-        draws=[parse_draw(d) for d in draws]
-    )
+    return Game(id=int(id), draws=[parse_draw(d) for d in draws])
+
 
 games = [parse_game(record) for record in data]
 
+
 def is_game_valid(game: Game) -> bool:
     for draw in game.draws:
-        if ((draw.blue or 0) > color_limits[Color.BLUE]) or ((draw.green or 0) > color_limits[Color.GREEN]) or ((draw.red or 0) > color_limits[Color.RED]):
+        if (
+            ((draw.blue or 0) > color_limits[Color.BLUE])
+            or ((draw.green or 0) > color_limits[Color.GREEN])
+            or ((draw.red or 0) > color_limits[Color.RED])
+        ):
             return False
 
     return True
+
 
 print("Part I Answer:", sum([g.id for g in games if is_game_valid(g)]))
 
@@ -68,5 +78,6 @@ def compute_power(game: Game) -> int:
     max_blue = max([draw.blue or 0 for draw in game.draws])
 
     return max_green * max_red * max_blue
+
 
 print("Part II Answer:", sum([compute_power(game) for game in games]))
--- python/day3.py
+++ python/day3.py
@@ -1,15 +1,31 @@
 import copy
 from typing import Optional, List, Tuple
 
+from dataclasses import dataclass
+
+from itertools import product
+from functools import reduce
+
+
+def flat_map(f, xs):
+    return reduce(lambda a, b: a + b, map(f, xs))
+
+
 with open("data/day3.txt", "r") as f:
     data = f.read().split("\n")
 
 numbers = [str(x) for x in range(10)]
-line_length = len(data[0])
 
-has_adjacent_symbol = {}
 
-def capture_number_from_starting_position(row: List[str], starting_position: int) -> Optional[Tuple[int, int, int]]:
+@dataclass
+class Coordinates:
+    row: int
+    column: int
+
+
+def capture_number_from_starting_position(
+    row: List[str], starting_position: int
+) -> Optional[Tuple[int, int, int]]:
     ## ..314...
     ## start: 4
     ## return: (3, 5, 314)
@@ -44,7 +60,8 @@ def capture_number_from_starting_position(row: List[str], starting_position: int
         if row[right_index] in numbers:
             captured += row[right_index]
 
-    return (left_index, right_index, captured)
+    return (int(left_index), int(right_index), int(captured))
+
 
 def determine_rows_to_check(data: List[List[str]], current_row: int) -> List[int]:
     if current_row == 0:
@@ -53,6 +70,7 @@ def determine_rows_to_check(data: List[List[str]], current_row: int) -> List[int
         return [current_row - 1, current_row]
     else:
         return [current_row - 1, current_row, current_row + 1]
+
 
 def determine_cols_to_check(row: List[str], current_col: int) -> List[int]:
     if current_col == 0:
@@ -63,10 +81,11 @@ def determine_cols_to_check(row: List[str], current_col: int) -> List[int]:
         return [current_col - 1, current_col, current_col + 1]
 
 
-
-def find_adjacent_numbers(data: List[List[str]], row: int, column: int) -> List[int]:
-    rows_to_check = determine_rows_to_check(data=data, current_row=row)
-    cols_to_check = determine_cols_to_check(row=data[row], current_col=column)
+def find_adjacent_numbers(data: List[List[str]], coords: Coordinates) -> List[int]:
+    rows_to_check = determine_rows_to_check(data=data, current_row=coords.row)
+    cols_to_check = determine_cols_to_check(
+        row=data[coords.row], current_col=coords.column
+    )
 
     result = [
         capture_number_from_starting_position(data[r], c)
@@ -74,19 +93,32 @@ def find_adjacent_numbers(data: List[List[str]], row: int, column: int) -> List[
         for c in cols_to_check
     ]
 
-    return {
-        r
-        for r in result
-        if r
-    }
+    return list({r for r in result if r})
 
 
+grid = list(
+    map(
+        lambda point: Coordinates(row=point[0], column=point[1]),
+        product(range(len(data)), range(len(data[0]))),
+    )
+)
 
-result = [
-    find_adjacent_numbers(data=data, row=r, column=c)
-    for r in range(len(data))
-    for c in range(len(data[r]))
-    if data[r][c] != "." and data[r][c] not in numbers
-]
 
-print(sum([int(record[2]) for r in result for record in r]))
+symbols = filter(
+    lambda coords: data[coords.row][coords.column] not in numbers + ["."], grid
+)
+
+part_1 = flat_map(
+    lambda coords: find_adjacent_numbers(data=data, coords=coords), symbols
+)
+
+print("Part I Solution:", sum(map(lambda record: int(record[2]), part_1)))
+
+gears = filter(lambda coords: data[coords.row][coords.column] == "*", grid)
+
+part_2 = filter(
+    lambda x: len(x) == 2,
+    map(lambda coords: find_adjacent_numbers(data=data, coords=coords), gears),
+)
+
+print("Part II Solution:", sum(map(lambda record: record[0][2] * record[1][2], part_2)))
