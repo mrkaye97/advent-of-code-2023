9f90e90e95d01999051755ff891b53dbea06fed0
Matt Kaye <mrkaye97@gmail.com>
2023-12-03 10:16:23 -0500

    much improved solution

--- python/day3.py
+++ python/day3.py
@@ -1,4 +1,5 @@
 import copy
+from typing import Optional, List, Tuple
 
 with open("data/day3.txt", "r") as f:
     data = f.read().split("\n")
@@ -8,59 +9,84 @@ line_length = len(data[0])
 
 has_adjacent_symbol = {}
 
-for row in range(len(data)):
-    for column in range(len(data[row])):
-        ul = data[row - 1][column - 1] if row > 0 and column > 0 else None
-        u = data[row - 1][column] if row > 0 else None
-        ur = data[row - 1][column + 1] if row > 0 and column < (len(data[row]) - 1) else None
-        l = data[row][column - 1] if column > 0 else None
-        r = data[row][column + 1] if column < (len(data[row]) - 1) else None
-        dl = data[row + 1][column - 1] if (row < (len(data) - 1)) and column > 0 else None
-        d = data[row + 1][column] if (row < (len(data) - 1)) else None
-        dr = data[row + 1][column + 1] if (row < (len(data) - 1)) and column < (len(data[row]) - 1) else None
+def capture_number_from_starting_position(row: List[str], starting_position: int) -> Optional[Tuple[int, int, int]]:
+    ## ..314...
+    ## start: 4
+    ## return: (3, 5, 314)
+    ## in format (start, end, value)
+
+    if row[starting_position] not in numbers:
+        return None
+
+    captured = ""
+    left_index = copy.deepcopy(starting_position)
+    while True:
+        left_index -= 1
+        if left_index < 0:
+            break
+
+        if row[left_index] not in numbers:
+            break
+
+        if row[left_index] in numbers:
+            captured = row[left_index] + captured
+
+    captured += row[starting_position]
+    right_index = copy.deepcopy(starting_position)
+    while True:
+        right_index += 1
+        if right_index >= len(row):
+            break
+
+        if row[right_index] not in numbers:
+            break
+
+        if row[right_index] in numbers:
+            captured += row[right_index]
+
+    return (left_index, right_index, captured)
+
+def determine_rows_to_check(data: List[List[str]], current_row: int) -> List[int]:
+    if current_row == 0:
+        return [current_row, current_row + 1]
+    elif current_row == len(data):
+        return [current_row - 1, current_row]
+    else:
+        return [current_row - 1, current_row, current_row + 1]
+
+def determine_cols_to_check(row: List[str], current_col: int) -> List[int]:
+    if current_col == 0:
+        return [current_col, current_col + 1]
+    elif current_col == len(row) - 1:
+        return [current_col - 1, current_col]
+    else:
+        return [current_col - 1, current_col, current_col + 1]
 
 
-        result = [
-            (ul and ul != "." and ul not in numbers),
-            (u and u != "." and u not in numbers),
-            (ur and ur != "." and ur not in numbers),
-            (l and l != "." and l not in numbers),
-            (r and r != "." and r not in numbers),
-            (dl and dl != "." and dl not in numbers),
-            (d and d != "." and d not in numbers),
-            (dr and dr != "." and dr not in numbers)
-        ]
 
-        has_adjacent_symbol[str(row) + "," + str(column)] = any(result)
+def find_adjacent_numbers(data: List[List[str]], row: int, column: int) -> List[int]:
+    rows_to_check = determine_rows_to_check(data=data, current_row=row)
+    cols_to_check = determine_cols_to_check(row=data[row], current_col=column)
 
+    result = [
+        capture_number_from_starting_position(data[r], c)
+        for r in rows_to_check
+        for c in cols_to_check
+    ]
 
-total = 0
-for row_ix, row in enumerate(data):
-    ix = 0
-    while ix < len(row) - 1:
-        char = row[ix]
-        if char not in numbers:
-            ix += 1
-        else:
-            inc = 0
-            start = copy.deepcopy(ix)
-            curr_char = ""
-            char_block = curr_char
-            while not curr_char or curr_char in numbers and start + inc < len(row):
-                curr_char = row[start + inc]
-
-                if not curr_char in numbers:
-                    break
-
-                char_block += curr_char
-                inc += 1
+    return {
+        r
+        for r in result
+        if r
+    }
 
 
-            any_touches_sym = any([has_adjacent_symbol[str(row_ix) + "," + str(c)] for c in range(start, start + inc)])
 
-            if any_touches_sym:
-                total += int(char_block)
+result = [
+    find_adjacent_numbers(data=data, row=r, column=c)
+    for r in range(len(data))
+    for c in range(len(data[r]))
+    if data[r][c] != "." and data[r][c] not in numbers
+]
 
-            ix += inc
-
-print(total)
+print(sum([int(record[2]) for r in result for record in r]))
