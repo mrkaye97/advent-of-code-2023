13e478f17779316c4120ca1d85cf8b4539fac8af
Matt Kaye <mrkaye97@gmail.com>
2023-12-08 09:27:20 -0500

    quadratic formula!

--- python/day6.py
+++ python/day6.py
@@ -1,4 +1,4 @@
-from math import prod
+from math import prod, sqrt, floor, ceil
 from functools import reduce
 
 with open("data/day6.txt", "r") as f:
@@ -10,14 +10,10 @@ def determine_num_winning_charge_times(race: tuple[str, str]) -> int:
     race_duration = int(race[0])
     race_record_distance = int(race[1])
 
-    for charge_time in range(race_duration + 1):
-        speed = charge_time
-        distance_traveled = speed * (race_duration - charge_time)
+    low = floor((-1 * race_duration + sqrt(race_duration**2 - 4 * (-1) * (-1 * race_record_distance))) / (-2))
+    high = ceil((-1 * race_duration - sqrt(race_duration**2 - 4 * (-1) * (-1 * race_record_distance))) / (-2))
 
-        if distance_traveled > race_record_distance:
-            return 1 + race_duration - 2 * charge_time
-
-    return 0
+    return high - low - 1
 
 print("Part I Solution:", prod(map(determine_num_winning_charge_times, data)))
 print("Part II Solution:", determine_num_winning_charge_times(reduce(lambda l, r: (l[0] + r[0], l[1] + r[1]), data)))
